abb0a79519a467be763676c690f1536746e88ad4
Pedro Foltram <pedroaugustofolb@gmail.com>
2023-03-15 10:47:27 -0300

    adicionando databases connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      // common database
+      'mongodb+srv://fasteng:dHjGSHOkTHRAwTN5@fasteng.fdugqs8.mongodb.net/common?retryWrites=true&w=majority',
+      { connectionName: 'common' },
+    ),
+    MongooseModule.forRoot(
+      // asphalt database
+      'mongodb+srv://fasteng:dHjGSHOkTHRAwTN5@fasteng.fdugqs8.mongodb.net/asphalt?retryWrites=true&w=majority',
+      { connectionName: 'asphalt' },
+    ),
+    MongooseModule.forRoot(
+      // soils database
+      'mongodb+srv://fasteng:dHjGSHOkTHRAwTN5@fasteng.fdugqs8.mongodb.net/soils?retryWrites=true&w=majority',
+      { connectionName: 'soils' },
+    ),
+    MongooseModule.forRoot(
+      // concrete database
+      'mongodb+srv://fasteng:dHjGSHOkTHRAwTN5@fasteng.fdugqs8.mongodb.net/concrete?retryWrites=true&w=majority',
+      { connectionName: 'concrete' },
+    ),
+  ],
   controllers: [],
   providers: [],
 })
