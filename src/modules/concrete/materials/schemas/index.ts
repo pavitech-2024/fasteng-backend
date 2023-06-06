b61066376b4827ca921a28e064f71bd6ee700267
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2023-06-06 19:00:41 -0300

    feature: concrete material module and respective crud implemented

--- src/modules/concrete/materials/schemas/index.ts
+++ src/modules/concrete/materials/schemas/index.ts
@@ -35,8 +35,8 @@ export class Material {
     recieveDate?: string;
     extractionDate?: string;
     collectionDate?: string;
-    // classification_CAP?: 'CAP 30/45' | 'CAP 50/70' | 'CAP 85/100' | 'CAP 150/200'; // for CAP
-    // classification_AMP?: 'AMP 50/65' | 'AMP 55/75' | 'AMP 60/85' | 'AMP 65/90'; // for AMP
+    classification_CAP?: 'CAP 30/45' | 'CAP 50/70' | 'CAP 85/100' | 'CAP 150/200'; // for CAP
+    classification_AMP?: 'AMP 50/65' | 'AMP 55/75' | 'AMP 60/85' | 'AMP 65/90'; // for AMP
     observation?: string;
   };
 }
