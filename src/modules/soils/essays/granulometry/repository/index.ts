08d6c652bf25708c3935952b7cceda3792b4db17
Gabriel Guimaraẽs de Almeida <gabriel.almeida@ccc.ufcg.edu.br>
2023-08-09 19:17:24 -0300

    Ajuste na verificação inicial dos ensaios de SUCS e Granulometria

--- src/modules/soils/essays/granulometry/repository/index.ts
+++ src/modules/soils/essays/granulometry/repository/index.ts
@@ -2,11 +2,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Granulometry, GranulometryDocument } from '../schemas';
 import { DATABASE_CONNECTION } from '../../../../../infra/mongoose/database.config';
 import { Model } from 'mongoose';
+import { Logger } from '@nestjs/common';
 
 export class GranulometryRepository {
+  private logger = new Logger(GranulometryRepository.name);
+
   constructor(@InjectModel(Granulometry.name, DATABASE_CONNECTION.SOILS) private granulometryModel: Model<GranulometryDocument>) {}
 
   async findOne(granulometryFilterQuery: any): Promise<Granulometry> {
+    this.logger.log(granulometryFilterQuery);
     return this.granulometryModel.findOne(granulometryFilterQuery);
   }
 
--- src/modules/soils/essays/sucs/repository/index.ts
+++ src/modules/soils/essays/sucs/repository/index.ts
@@ -2,11 +2,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Sucs, SucsDocument } from '../schemas';
 import { DATABASE_CONNECTION } from '../../../../../infra/mongoose/database.config';
 import { Model } from 'mongoose';
+import { Logger } from '@nestjs/common';
 
 export class SucsRepository {
+  private logger = new Logger(SucsRepository.name);
+
   constructor(@InjectModel(Sucs.name, DATABASE_CONNECTION.SOILS) private sucsModel: Model<SucsDocument>) {}
 
   async findOne(sucsFilterQuery: any): Promise<Sucs> {
+    this.logger.log(sucsFilterQuery);
     return this.sucsModel.findOne(sucsFilterQuery);
   }
 
