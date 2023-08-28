177bc5d0f8007e09afa237225783ff2bfcacfdda
Gabriel Guimaraẽs de Almeida <gabriel.almeida@ccc.ufcg.edu.br>
2023-08-28 15:46:32 -0300

    Ajustando imports

--- src/modules/soils/essays/sucs/sucs.module.ts
+++ src/modules/soils/essays/sucs/sucs.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SucsController } from './controller';
-import { SucsService } from './service';
+import { SucsService } from './service/index';
 import { SucsRepository } from './repository';
 import { GeneralData_SUCS_Service } from './service/general-data.sucs.service';
 import { Calc_SUCS_Service } from './service/calc.sucs.service';
