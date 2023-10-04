c40711bd14ab34949da163fd4f1b56aeb782b0f3
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2023-10-03 21:34:33 -0300

    chore: commiting data

--- src/modules/asphalt/asphalt.module.ts
+++ src/modules/asphalt/asphalt.module.ts
@@ -4,15 +4,18 @@ import { DATABASE_CONNECTION } from '../../infra/mongoose/database.config';
 import { MaterialsModule } from './materials/materials.module';
 import { Material, MaterialSchema } from './materials/schemas';
 import { Rtfo, RtfoSchema } from './essays/rtfo/schemas';
+import { RtfoModule } from './essays/rtfo/rtfo.module';
 
 const Models: ModelDefinition[] = [
   { name: Material.name, schema: MaterialSchema },
   { name: Rtfo.name, schema: RtfoSchema },
 ];
 
+const Modules = [MaterialsModule, RtfoModule];
+
 @Global()
 @Module({
-  imports: [MongooseModule.forFeature(Models, DATABASE_CONNECTION.ASPHALT), MaterialsModule],
-  exports: [MongooseModule, MaterialsModule],
+  imports: [MongooseModule.forFeature(Models, DATABASE_CONNECTION.ASPHALT), ...Modules],
+  exports: [MongooseModule, ...Modules],
 })
 export class AsphaltModule {}
