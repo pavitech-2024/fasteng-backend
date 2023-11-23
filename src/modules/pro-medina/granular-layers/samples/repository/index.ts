b56f0d76b7f144929c825c0bbf7a06809b386d49
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2023-11-22 22:08:38 -0300

    feat: filter route fully integrated

--- src/modules/pro-medina/granular-layers/samples/repository/index.ts
+++ src/modules/pro-medina/granular-layers/samples/repository/index.ts
@@ -24,20 +24,34 @@ export class GranularLayers_SamplesRepository {
     const formattedLimit = Number(limit);
     const skip = (fomattedPage - 1) * formattedLimit;
     const parsedFilter = JSON.parse(filter);
-    const searchFilter = { $and: [...parsedFilter] };
     const sortParam = sort ? sort[0] : "";
+
+    let formattedFilter = [];
+
+    parsedFilter.forEach(obj => {
+      if (obj.name) formattedFilter.push({ 'generalData.name': obj.name });
+      if (obj.cityState) formattedFilter.push({ 'generalData.cityState': obj.cityState });
+      if (obj.zone) formattedFilter.push({ 'generalData.zone': obj.zone });
+      if (obj.layer) formattedFilter.push({ 'generalData.layer': obj.layer });
+    });
+
     const docs = await this.granularLayers_sampleModel
-      .find(searchFilter)
+      .find({
+        $and: formattedFilter
+      })
       .skip(skip)
-      .sort(sortParam)
       .limit(limit)
       .lean();
+
     
-    const count = await this.granularLayers_sampleModel.countDocuments(searchFilter);
-    let totalPages
+    const count = await this.granularLayers_sampleModel.countDocuments({
+      $and: formattedFilter
+    });
+
+    let totalPages;
 
     if (need_count) {
-      totalPages = Math.ceil(count / limit);
+      totalPages = Math.ceil(count / formattedLimit);
     }
 
     return {
