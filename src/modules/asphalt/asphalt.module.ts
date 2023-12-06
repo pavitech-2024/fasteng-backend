7a6e7829c60c6e7999581fd8959bcb0d8ac59670
Gabriel Guimaraẽs de Almeida <gabriel.almeida@ccc.ufcg.edu.br>
2023-12-06 03:22:56 -0300

    recuperação elástica

--- src/modules/asphalt/asphalt.module.ts
+++ src/modules/asphalt/asphalt.module.ts
@@ -33,6 +33,8 @@ import { SofteningPoint, SofteningPointSchema } from './essays/softeningPoint/sc
 import { SofteningPointModule } from './essays/softeningPoint/softeningPoint.module';
 import { Ddui, DduiSchema } from './essays/ddui/schemas';
 import { DduiModule } from './essays/ddui/ddui.module';
+import { ElasticRecovery, ElasticRecoverySchema } from './essays/elasticRecovery/schema';
+import { ElasticRecoveryModule } from './essays/elasticRecovery/elasticRecovery.module';
 
 const Models: ModelDefinition[] = [
   { name: Material.name, schema: MaterialSchema },
@@ -51,6 +53,7 @@ const Models: ModelDefinition[] = [
   { name: Ddui.name, schema: DduiSchema },
   { name: ShapeIndex.name, schema: ShapeIndexSchema },
   { name: ElongatedParticles.name, schema: ElongatedParticlesSchema },
+  { name: ElasticRecovery.name, schema: ElasticRecoverySchema },
 ];
 
 const Modules = [
@@ -70,11 +73,12 @@ const Modules = [
   DduiModule,
   ShapeIndexModule, 
   ElongatedParticlesModule,
+  ElasticRecoveryModule,
 ]
 
 @Global()
 @Module({
-  imports: [MongooseModule.forFeature(Models, DATABASE_CONNECTION.ASPHALT), MaterialsModule],
+  imports: [MongooseModule.forFeature(Models, DATABASE_CONNECTION.ASPHALT), ...Modules],
   exports: [MongooseModule, MaterialsModule],
 })
 
