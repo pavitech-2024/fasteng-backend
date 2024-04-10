38956117b8f78205958bbe59b1f25ae0b827a035
unknown <teixeirahist1988@gmail.com>
2024-04-10 19:48:03 -0300

    chore: saveing

--- src/modules/asphalt/dosages/marshall/service/material-selection.marshall.service.ts
+++ src/modules/asphalt/dosages/marshall/service/material-selection.marshall.service.ts
@@ -1,52 +1,57 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { AsphaltGranulometryRepository } from "../../../../../modules/asphalt/essays/granulometry/repository";
-import { SpecifyMassRepository } from "../../../../../modules/asphalt/essays/specifyMass/repository";
-import { MaterialsRepository } from "../../../../../modules/asphalt/materials/repository";
-import { Model } from "mongoose";
-import { MarshallRepository } from "../repository";
-import { Marshall, MarshallDocument } from "../schemas";
-import { InjectModel } from "@nestjs/mongoose";
-import { DATABASE_CONNECTION } from "infra/mongoose/database.config";
+import { Injectable, Logger } from '@nestjs/common';
+import { AsphaltGranulometryRepository } from '../../../../../modules/asphalt/essays/granulometry/repository';
+import { SpecifyMassRepository } from '../../../../../modules/asphalt/essays/specifyMass/repository';
+import { MaterialsRepository } from '../../../../../modules/asphalt/materials/repository';
+import { Model } from 'mongoose';
+import { MarshallRepository } from '../repository';
+import { Marshall, MarshallDocument } from '../schemas';
+import { InjectModel } from '@nestjs/mongoose';
+import { DATABASE_CONNECTION } from 'infra/mongoose/database.config';
 
 @Injectable()
 export class MaterialSelection_Marshall_Service {
-  private logger = new Logger(MaterialSelection_Marshall_Service.name)
+  private logger = new Logger(MaterialSelection_Marshall_Service.name);
 
   constructor(
-    @InjectModel(Marshall.name, DATABASE_CONNECTION.ASPHALT) 
+    @InjectModel(Marshall.name, DATABASE_CONNECTION.ASPHALT)
     private marshallModel: Model<MarshallDocument>,
     private readonly material_repository: MaterialsRepository,
     private readonly granulometry_repository: AsphaltGranulometryRepository,
     private readonly specifyMass_repository: SpecifyMassRepository,
     private readonly marshallRepository: MarshallRepository,
-  ) { }
+  ) {}
 
   async getMaterials(userId: string) {
     try {
+      this.logger.log('get materials on material-selection.marshall.service.ts > [body]', { userId: userId });
+      
       const materials = await this.material_repository.findByUserId({
-        "userId": userId,
+        userId: userId,
       });
-
 
       const granulometrys = await this.granulometry_repository.findAll();
       const specifyMasses = await this.specifyMass_repository.findAll();
 
       const binders = materials.filter(({ type }) => {
-        return type === 'CAP' || type === 'asphaltBinder'
+        return type === 'CAP' || type === 'asphaltBinder';
       });
 
       const aggregates = materials.filter(({ _id, type }) => {
-        if (type === 'CAP' || type === 'asphaltBinder') return false
-        const granulometry = granulometrys.some(({ generalData }) => {
-          const { material } = generalData
-          return _id.toString() === material._id.toString()
-        });
-        const specifyMass = specifyMasses.some(({ generalData }) => {
-          const { material } = generalData
-          return _id.toString() === material._id.toString()
-        });
-        return granulometry && specifyMass;
+        if (type === 'CAP' || type === 'asphaltBinder') return false;
 
+        // Find the materials that already has the granulometry essay completed;
+        const granulometry = granulometrys.some(({ generalData }) => {
+          const { material } = generalData;
+          return _id.toString() === material._id.toString();
+        });
+
+        // Find the materials that already has the specific mass essay completed;
+        const specifyMass = specifyMasses.some(({ generalData }) => {
+          const { material } = generalData;
+          return _id.toString() === material._id.toString();
+        });
+
+        return granulometry && specifyMass;
       });
 
       const filteredMaterials = binders.concat(aggregates);
@@ -69,10 +74,7 @@ export class MaterialSelection_Marshall_Service {
 
       const marshallWithMaterials = { ...marshallExists._doc, materialSelectionData: materialDataWithoutName };
 
-      await this.marshallModel.updateOne(
-        { _id: marshallExists._doc._id },
-        marshallWithMaterials
-      );
+      await this.marshallModel.updateOne({ _id: marshallExists._doc._id }, marshallWithMaterials);
 
       if (marshallExists._doc.generalData.step < 2) {
         await this.marshallRepository.saveStep(marshallExists, 2);
@@ -80,7 +82,7 @@ export class MaterialSelection_Marshall_Service {
 
       return true;
     } catch (error) {
-      throw error
+      throw error;
     }
   }
 }
