006e0c08f1f72e93162bf810ee5df7091ace1199
unknown <teixeirahist1988@gmail.com>
2024-04-24 17:32:04 -0300

    chore: save

--- src/modules/asphalt/materials/service/get-essays-by-material.service.ts
+++ src/modules/asphalt/materials/service/get-essays-by-material.service.ts
@@ -10,6 +10,13 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Material, MaterialDocument } from '../schemas';
 import { DATABASE_CONNECTION } from 'infra/mongoose/database.config';
 import { Model } from 'mongoose';
+import { SandEquivalentRepository } from 'modules/asphalt/essays/sandEquivalent/repository';
+import { AngularityRepository } from 'modules/asphalt/essays/angularity/repository';
+import { ViscosityRotationalRepository } from 'modules/asphalt/essays/viscosityRotational/repository';
+import { PenetrationRepository } from 'modules/asphalt/essays/penetration/repository';
+import { SofteningPointRepository } from 'modules/asphalt/essays/softeningPoint/repository';
+import { FlashPointRepository } from 'modules/asphalt/essays/flashPoint/repository';
+import { DuctilityRepository } from 'modules/asphalt/essays/ductility/repository';
 
 @Injectable()
 export class GetEssaysByMaterial_Service {
@@ -25,6 +32,13 @@ export class GetEssaysByMaterial_Service {
     private readonly specificMassRepository: SpecifyMassRepository,
     private readonly shapeIndexRepository: ShapeIndexRepository,
     private readonly losAngelesAbrasionRepository: AbrasionRepository,
+    private readonly sandEquivalentRepository: SandEquivalentRepository,
+    private readonly angularityRepository: AngularityRepository,
+    private readonly viscosityRotationalRepository: ViscosityRotationalRepository,
+    private readonly penetrationRepository: PenetrationRepository,
+    private readonly softeningPointRepository: SofteningPointRepository,
+    private readonly flashPointRepository: FlashPointRepository,
+    private readonly ductilityRepository: DuctilityRepository
   ) {}
 
   async getEssaysByMaterial(material: any) {
@@ -77,7 +91,49 @@ export class GetEssaysByMaterial_Service {
           case 'shapeIndex':
             response = await this.shapeIndexRepository.findOne({ 'generalData.material._id': _id.toString() });
             if (response) {
-              essay = { essayName, data: response }
+              essay = { essayName, data: response };
+            }
+            break;
+          case 'sandEquivalent':
+            response = await this.sandEquivalentRepository.findOne({ 'generalData.material._id': _id.toString() });
+            if (response) {
+              essay = { essayName, data: response };
+            }
+            break;
+          case 'angularity':
+            response = await this.angularityRepository.findOne({ 'generalData.material._id': _id.toString() });
+            if (response) {
+              essay = { essayName, data: response };
+            }
+            break;
+          case 'viscosityRotational':
+            response = await this.viscosityRotationalRepository.findOne({ 'generalData.material._id': _id.toString() });
+            if (response) {
+              essay = { essayName, data: response };
+            }
+            break;
+          case 'penetration':
+            response = await this.penetrationRepository.findOne({ 'generalData.material._id': _id.toString() });
+            if (response) {
+              essay = { essayName, data: response };
+            }
+            break;
+          case 'softeningPoint':
+            response = await this.softeningPointRepository.findOne({ 'generalData.material._id': _id.toString() });
+            if (response) {
+              essay = { essayName, data: response };
+            }
+            break;
+          case 'flashPoint':
+            response = await this.flashPointRepository.findOne({ 'generalData.material._id': _id.toString() });
+            if (response) {
+              essay = { essayName, data: response };
+            }
+            break;
+          case 'ductility':
+            response = await this.ductilityRepository.findOne({ 'generalData.material._id': _id.toString() });
+            if (response) {
+              essay = { essayName, data: response };
             }
             break;
           default:
@@ -115,26 +171,20 @@ export class GetEssaysByMaterial_Service {
         ];
         break;
       case 'fineAggregate':
-        possiblesExperimentTypes = [
-          'Granulometry',
-          'Fine Specific Gravity',
-          'Sand Equivalent',
-          'realDensity',
-          'Angularity',
-        ];
+        possiblesExperimentTypes = ['granulometry', 'specificMass', 'sandEquivalent', 'realDensity', 'angularity'];
         break;
       case 'filler':
-        possiblesExperimentTypes = ['Granulometry', 'Fine Specific Gravity', 'Sand Equivalent'];
+        possiblesExperimentTypes = ['granulometry', 'specificMass', 'sandEquivalent'];
         break;
       case 'asphaltBinder':
         possiblesExperimentTypes = [
-          'Viscosity',
-          'Penetration',
-          'Softening Point',
-          'Flash Point',
-          'Ductility',
-          'Dosage',
-          'Rtfo',
+          'viscosityRotational',
+          'penetration',
+          'softeningPoint',
+          'flashPoint',
+          'ductility',
+          'dosage',
+          'rtfo',
         ];
         break;
       case 'modifiedAsphaltBinder':
