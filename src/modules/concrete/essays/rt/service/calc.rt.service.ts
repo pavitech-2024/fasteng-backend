b8cd37fad99438491297c5cac0a1fb3f4d26cb42
pavitech <pavitechjbr@gmail.com>
2024-10-31 15:44:24 -0300

    feat: concrete rt essay partially implemented

--- src/modules/concrete/essays/rt/service/calc.rt.service.ts
+++ src/modules/concrete/essays/rt/service/calc.rt.service.ts
@@ -1,80 +1,101 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { Calc_Concrete_RT_Dto, Calc_Concrete_RT_Out } from "../dto/calc.rt.dto";
-
+import { Injectable, Logger } from '@nestjs/common';
+import { Calc_Concrete_RT_Dto, Calc_Concrete_RT_Out } from '../dto/calc.rt.dto';
+import { ConcreteRtInterpolationDto } from '../dto/concrete-rt-interpolation.dto';
+import { Calc_CONCRETERC_Dto } from '../../rc/dto/calc.rc.dto';
 
 @Injectable()
 export class Calc_ConcreteRt_Service {
   private logger = new Logger(Calc_ConcreteRt_Service.name);
 
-  private confirmResults(data: any[], pressConstant: number) {
-    const {
-      rtsMpa: everyRtsMpa,
-      rtsKgf: everyRtsKgf,
-      average: average,
-    } = this.calculateRtsValues(data, Number(pressConstant));
-
-    return { everyRtsMpa, everyRtsKgf, average };
-  }
-
-  async calculateConcreteRt({ step2Data, step3Data }: Calc_Concrete_RT_Dto): Promise<{ success: boolean; result: Calc_Concrete_RT_Out }> {
+  async calculateConcreteRtInterpolation({
+    lowerReference,
+    higherReference,
+    age,
+    tolerance,
+  }: ConcreteRtInterpolationDto) {
     try {
-      this.logger.log('calculate concreteRt on calc.concreteRt.service.ts > [body]')
+      this.logger.log('calculate rt interpolation on calc.rc.interpolation.service.ts > [body]');
 
-      const result = this.confirmResults(step3Data.data, step2Data.pressConstant);
-
-      return {
-        success: true,
-        result: result,
+      let result = {
+        data: 0,
+        isPermited: true,
       };
+
+      const higherReferenceArr = Object.values(higherReference).map((e) => e);
+      const lowerReferenceArr = Object.values(lowerReference).map((e) => e);
+
+      const ruptureAgeRef = higherReferenceArr[0] - lowerReferenceArr[0];
+      const toleranceRef = higherReferenceArr[1] - lowerReferenceArr[1];
+
+      const ageInput = higherReferenceArr[0] - age;
+      const ageRatio = ruptureAgeRef / ageInput;
+
+      const toleranceValue = (ageRatio * higherReferenceArr[1]) / toleranceRef;
+
+      const toleranceRatio = toleranceValue / higherReferenceArr[1];
+
+      // Condicional: apenas se o usuário inserir valor de tolerância no input do step2;
+
+      // Verificação da margem de erro (10+-);
+      if (tolerance) {
+        if (toleranceRatio >= tolerance - 10 || toleranceRatio <= tolerance + 10) {
+          result.isPermited = true;
+        } else {
+          result.isPermited = false;
+        }
+      }
+
+      result.data = toleranceValue;
+
+      return result;
     } catch (error) {
       throw error;
     }
   }
 
-  private calculateRtsValues(data: any[], pressConst: number) {
-    const rtsMpa: number[] = [];
-    const rtsKgf: number[] = [];
+  async calculateConcreteRt({
+    step3Data,
+  }: Calc_Concrete_RT_Dto): Promise<{ success: boolean; result: any }> {
+    try {
+      this.logger.log('calculate rt on calc.rc.service.ts > [body]');
 
-    data.forEach((row) => {
-      let sumDiameter = 0;
-      let nDiameter = 0;
-      let sumHeight = 0;
-      let nHeight = 0;
+      const { supportsDistance, appliedCharge } = step3Data;
 
-      Object.keys(row).forEach((key) => {
-        if (String(key)[0] === 'd') {
-          if (row[key] !== null) {
-            sumDiameter += row[key];
-            nDiameter++;
-          }
-        }
-        if (String(key)[0] === 'h') {
-          if (row[key] !== null) {
-            sumHeight += row[key];
-            nHeight++;
-          }
-        }
-      });
+      const flexualTensileStrength = this.calculateFlexualTensileStrength(
+        appliedCharge,
+        supportsDistance,
+      );
 
-      const averageDiameter = sumDiameter / nDiameter;
-      const averageHeight = sumHeight / nHeight;
+      const compressionResistance = this.calculateCompressionResistance(appliedCharge);
 
-      const rtMpa: number =
-        (2 * pressConst * row['pressReading']) / (Math.PI * (averageDiameter / 10) * (averageHeight / 10)) / 10;
-      const rtKgf = rtMpa * 10;
+      const result = {
+        flexualTensileStrength,
+        compressionResistance,
+      };
 
-      rtsMpa.push(rtMpa);
-      rtsKgf.push(rtKgf);
-    });
-
-    let totalRt = 0;
-
-    for (const value of rtsMpa) {
-      totalRt += value;
+      return {
+        success: true,
+        result,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        result: null,
+      };
     }
+  }
 
-    const average = totalRt / rtsMpa.length;
+  calculateFlexualTensileStrength(appliedCharge: number, supportsDistance: number) {
+    const chargeSupportsProduct = appliedCharge * supportsDistance;
+    const finalProduct = 1.5 * chargeSupportsProduct;
+    const flexualTensileStrength = finalProduct / 64000;
 
-    return { rtsMpa, rtsKgf, average };
+    return flexualTensileStrength;
+  }
+
+  calculateCompressionResistance(appliedCharge: number) {
+    const compressionResistance = appliedCharge / 1600;
+
+    return compressionResistance;
   }
 }
