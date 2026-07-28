c6e8561c2e2d47930b644024f272ffd3282d4be4
João Marco de Morais <koishikomeijijm213@gmail.com>
2026-07-28 11:13:24 -0300

    Fwd-Correção
    Correção da conexão do FWD

--- src/modules/asphalt/essays/fwd/fwd.module.ts
+++ src/modules/asphalt/essays/fwd/fwd.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+  import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FwdAnalysis, FwdAnalysisSchema } from '../fwd/schema/fwd.schema';
 import { FwdService } from '../fwd/services/fwd.service';
